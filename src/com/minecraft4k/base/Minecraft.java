ff9ac9d81c66d3e28e3d1038e725c203d504a4a0
HeathLoganCampbell <GitHub@HeathLoganCampbell.com>
2019-06-15 15:07:43 +1200

    Rename a few variables

--- src/com/minecraft4k/base/Minecraft.java
+++ src/com/minecraft4k/base/Minecraft.java
@@ -36,24 +36,28 @@ extends Applet implements Runnable {
     {
         try 
         {                             
-            float f1 = 64 + 32.5f;
-            float f2 = 65.0f;
-            float f3 = 96.5f;
-            float f4 = 0.0f;
-            float f5 = 0.0f;
-            float f6 = 0.0f;
+            float playerX = 64 + 32.5f;
+            float playerY = 65.0f;
+            float playerZ = 96.5f;
+            
+            float xVelocity = 0.0f;
+            float zVelocity = 0.0f;
+            float yVelocity = 0.0f;
+            
             long now = System.currentTimeMillis();
-            int i4 = -1;
+            int selectedBlock = -1;
             int i5 = 0;
-            float f7 = 0.0f;
-            float f8 = 0.0f;
+            
+            float yaw = 0.0f;
+            float pitch = 120.0f;
             while(true)
             {
                 int i11;
-                float sinf7 = (float)Math.sin(f7);
-                float cosf7 = (float)Math.cos(f7);
-                float f11 = (float)Math.sin(f8);
-                float f12 = (float)Math.cos(f8);
+                float sinf7 = (float)Math.sin(yaw);
+                float cosf7 = (float)Math.cos(yaw);
+                float sinf8 = (float)Math.sin(pitch);
+                float cosf8 = (float)Math.cos(pitch);
+                
                 block7 : while (System.currentTimeMillis() - now > 10) {
                     float f13;
                     float f14;
@@ -65,138 +69,175 @@ extends Applet implements Runnable {
                             f15 = 0.0f;
                         }
                         if (f15 > 0.0f) {
-                            f7 += f13 * f15 / 400.0f;
-                            if ((f8 -= f14 * f15 / 400.0f) < -1.57f) {
-                                f8 = -1.57f;
+                            yaw += f13 * f15 / 400.0f;
+                            if ((pitch -= f14 * f15 / 400.0f) < -1.57f) {
+                                pitch = -1.57f;
                             }
-                            if (f8 > 1.57f) {
-                                f8 = 1.57f;
+                            if (pitch > 1.57f) {
+                                pitch = 1.57f;
                             }
                         }
                     }
                     now += 10;
                     f13 = 0.0f;
                     f14 = 0.0f;
-                    f4 *= 0.5f;
-                    f5 *= 0.99f;
-                    f6 *= 0.5f;
-                    f4 += sinf7 * (f14 += (float)(this.inputData[119] - this.inputData[115]) * 0.02f) + cosf7 * (f13 += (float)(this.inputData[100] - this.inputData[97]) * 0.02f);
-                    f6 += cosf7 * f14 - sinf7 * f13;
-                    f5 += 0.003f;
-                    int i8 = 0;
-                    while (i8 < 3) {
-                        float f16 = f1 + f4 * (float)((i8 + 2) % 3 / 2);
-                        float f17 = f2 + f5 * (float)((i8 + 1) % 3 / 2);
-                        float f19 = f3 + f6 * (float)((i8 + 2) % 3 / 2);
+                    
+                    xVelocity *= 0.5f;
+                    zVelocity *= 0.99f;
+                    yVelocity *= 0.5f;
+                    
+                    xVelocity += sinf7 * (f14 += (float)(this.inputData[119] - this.inputData[115]) * 0.02f) + cosf7 * (f13 += (float)(this.inputData[100] - this.inputData[97]) * 0.02f);
+                    yVelocity += cosf7 * f14 - sinf7 * f13;
+                    zVelocity += 0.003f;
+                    
+                    int collAxis = 0;
+                    //collision code
+    
+                    //x = 0, y = 1, z = 2
+                    while (collAxis < 3)
+                    {
+                        float f16 = playerX + xVelocity * (float)((collAxis + 2) % 3 / 2);
+                        float f17 = playerY + zVelocity * (float)((collAxis + 1) % 3 / 2);
+                        float f19 = playerZ + yVelocity * (float)((collAxis + 2) % 3 / 2);
                         int i12 = 0;
-                        while (i12 < 12) {
+                        
+                        //height
+                        while (i12 < 12)
+                        {
                             int i13 = (int)(f16 + (float)(i12 >> 0 & 1) * 0.6f - 0.3f) - 64;
                             int i14 = (int)(f17 + (float)((i12 >> 2) - 1) * 0.8f + 0.65f) - 64;
                             int i15 = (int)(f19 + (float)(i12 >> 1 & 1) * 0.6f - 0.3f) - 64;
-                            if (i13 < 0 || i14 < 0 || i15 < 0 || i13 >= 64 || i14 >= 64 || i15 >= 64 || world.blockData[i13 + i14 * 64 + i15 * 4096] > 0) {
-                                if (i8 != 1) continue block7;
-                                if (this.inputData[32] > 0 && f5 > 0.0f) {
+                            //
+                            if (i13 < 0 || i14 < 0 || i15 < 0 || i13 >= 64 || i14 >= 64 || i15 >= 64 || world.blockData[i13 + i14 * 64 + i15 * 4096] > 0)
+                            {
+                            	//onlychecks for z axis
+                                if (collAxis != 1) continue block7;
+                               //Jumping code, if space
+                                if (this.inputData[32] > 0 && zVelocity > 0.0f)
+                                {
                                     this.inputData[32] = 0;
-                                    f5 = -0.1f;
+                                    zVelocity = -0.1f;
                                     continue block7;
                                 }
-                                f5 = 0.0f;
+                                zVelocity = 0.0f;
                                 continue block7;
                             }
                             ++i12;
                         }
-                        f1 = f16;
-                        f2 = f17;
-                        f3 = f19;
-                        ++i8;
+                        playerX = f16;
+                        playerY = f17;
+                        playerZ = f19;
+                        ++collAxis;
                     }
                 }
                 int i6 = 0;
                 int i7 = 0;
-                if (this.inputData[1] > 0 && i4 > 0) {
-                    world.blockData[i4] = 0;
+                if (this.inputData[1] > 0 && selectedBlock > 0)
+                {
+                    world.blockData[selectedBlock] = 0;
                     this.inputData[1] = 0;
                 }
-                if (this.inputData[0] > 0 && i4 > 0) {
-                    world.blockData[i4 + i5] = 1;
+                if (this.inputData[0] > 0 && selectedBlock > 0)
+                {
+                    world.blockData[selectedBlock + i5] = 1;
                     this.inputData[0] = 0;
                 }
                 int i8 = 0;
+                //delete collision
                 while (i8 < 12) {
-                    int i9 = (int)(f1 + (float)(i8 >> 0 & 1) * 0.6f - 0.3f) - 64;
-                    int i10 = (int)(f2 + (float)((i8 >> 2) - 1) * 0.8f + 0.65f) - 64;
-                    i11 = (int)(f3 + (float)(i8 >> 1 & 1) * 0.6f - 0.3f) - 64;
+                    int i9 = (int)(playerX + (float)(i8 >> 0 & 1) * 0.6f - 0.3f) - 64;
+                    int i10 = (int)(playerY + (float)((i8 >> 2) - 1) * 0.8f + 0.65f) - 64;
+                    i11 = (int)(playerZ + (float)(i8 >> 1 & 1) * 0.6f - 0.3f) - 64;
                     if (i9 >= 0 && i10 >= 0 && i11 >= 0 && i9 < 64 && i10 < 64 && i11 < 64) {
                         world.blockData[i9 + i10 * 64 + i11 * 4096] = 0;
                     }
                     ++i8;
                 }
-                float i82 = -1.0f;
+                float tempSelectingBlock = -1.0f;
                 int i9 = 0;
+                //render
                 while (i9 < 214) {
                     float f18 = (float)(i9 - 107) / 90.0f;
                     i11 = 0;
                     while (i11 < 120) {
                         float f20 = (float)(i11 - 60) / 90.0f;
                         float f21 = 1.0f;
-                        float f22 = f21 * f12 + f20 * f11;//dot product
-                        float f23 = f20 * f12 - f21 * f11;
+                        
+                        //rotation matrix
+                        float f22 = f21 * cosf8 + f20 * sinf8;
+                        float f23 = f20 * cosf8 - f21 * sinf8;
                         float f24 = f18 * cosf7 + f22 * sinf7;
                         float f25 = f22 * cosf7 - f18 * sinf7;
+                        
                         int i16 = 0;
                         int i17 = 255;
-                        double d = 20.0;
-                        float f26 = 5.0f;
-                        int i18 = 0;
-                        while (i18 < 3) {
+                        
+                        double fogOfWar = 20.0;//render distance
+                        float readDistance = 5.0f;//read distance
+                        int axis = 0;//
+                        while (axis < 3) {
                             float f27 = f24;
-                            if (i18 == 1) {
+                            if (axis == 1) {
                                 f27 = f23;
                             }
-                            if (i18 == 2) {
+                            if (axis == 2) {
                                 f27 = f25;
                             }
-                            float f28 = 1.0f / (f27 < 0.0f ? - f27 : f27);
+                            float f28 = 1.0f / Math.abs(f27);
                             float f29 = f24 * f28;
                             float f30 = f23 * f28;
                             float f31 = f25 * f28;
-                            float f32 = f1 - (float)((int)f1);
-                            if (i18 == 1) {
-                                f32 = f2 - (float)((int)f2);
+                            float f32 = playerX - (float)((int)playerX);
+                            
+                            if (axis == 1) 
+                            {
+                                f32 = playerY - (float)((int)playerY);
                             }
-                            if (i18 == 2) {
-                                f32 = f3 - (float)((int)f3);
+                            
+                            if (axis == 2) 
+                            {
+                                f32 = playerZ - (float)((int)playerZ);
                             }
-                            if (f27 > 0.0f) {
+                            
+                            if (f27 > 0.0f) 
+                            {
                                 f32 = 1.0f - f32;
                             }
                             float f33 = f28 * f32;
-                            float f34 = f1 + f29 * f32;
-                            float f35 = f2 + f30 * f32;
-                            float f36 = f3 + f31 * f32;
+                            float f34 = playerX + f29 * f32;
+                            float f35 = playerY + f30 * f32;
+                            float f36 = playerZ + f31 * f32;
                             if (f27 < 0.0f) 
                             {
-                                if (i18 == 0) {
+                                if (axis == 0) {
                                     f34 -= 1.0f;
                                 }
-                                if (i18 == 1) {
+                                if (axis == 1) {
                                     f35 -= 1.0f;
                                 }
-                                if (i18 == 2) {
+                                if (axis == 2) {
                                     f36 -= 1.0f;
                                 }
                             }
-                            while ((double)f33 < d) {
+                            while ((double)f33 < fogOfWar) {
+                            	//render all blocks
+                            	//off set
                                 int i21 = (int)f34 - 64;
                                 int i22 = (int)f35 - 64;
                                 int i23 = (int)f36 - 64;
+                                
+                                //block out of mapsize
                                 if (i21 < 0 || i22 < 0 || i23 < 0 || i21 >= 64 || i22 >= 64 || i23 >= 64) break;
+                                
                                 int i24 = i21 + i22 * 64 + i23 * 4096;
-                                int i25 = world.blockData[i24];
+                                int i25 = world.blockData[i24];//selected block
                                 if (i25 > 0) {
+                                	//render horz of block
                                     i6 = (int)((f34 + f36) * 16.0f) & 15;
+                                    //render vert of block
                                     i7 = ((int)(f35 * 16.0f) & 15) + 16;
-                                    if (i18 == 1) {
+                                    
+                                    if (axis == 1) {
                                         i6 = (int)(f34 * 16.0f) & 15;
                                         i7 = (int)(f36 * 16.0f) & 15;
                                         if (f30 < 0.0f) {
@@ -204,23 +245,23 @@ extends Applet implements Runnable {
                                         }
                                     }
                                     int i26 = 16777215;
-                                    if (i24 != i4 || i6 > 0 && i7 % 16 > 0 && i6 < 15 && i7 % 16 < 15) {
+                                    if (i24 != selectedBlock || i6 > 0 && i7 % 16 > 0 && i6 < 15 && i7 % 16 < 15) {
                                         i26 = textureData[i6 + i7 * 16 + i25 * 256 * 3];
                                     }
-                                    if (f33 < f26 && i9 == this.inputData[2] / 4 && i11 == this.inputData[3] / 4) {
-                                        i82 = i24;
+                                    if (f33 < readDistance && i9 == this.inputData[2] / 4 && i11 == this.inputData[3] / 4) {
+                                        tempSelectingBlock = i24;
                                         i5 = 1;
                                         if (f27 > 0.0f) {
                                             i5 = -1;
                                         }
-                                        i5 <<= 6 * i18;
-                                        f26 = f33;
+                                        i5 <<= 6 * axis;
+                                        readDistance = f33;
                                     }
                                     if (i26 > 0) {
                                         i16 = i26;
                                         i17 = 255 - (int)(f33 / 20.0f * 255.0f);
-                                        i17 = i17 * (255 - (i18 + 2) % 3 * 50) / 255;
-                                        d = f33;
+                                        i17 = i17 * (255 - (axis + 2) % 3 * 50) / 255;
+                                        fogOfWar = f33;
                                     }
                                 }
                                 f34 += f29;
@@ -228,7 +269,7 @@ extends Applet implements Runnable {
                                 f36 += f31;
                                 f33 += f28;
                             }
-                            ++i18;
+                            ++axis;
                         }
                         int r = (i16 >> 16 & 255) * i17 / 255;
                         int g = (i16 >> 8 & 255) * i17 / 255;
@@ -238,7 +279,7 @@ extends Applet implements Runnable {
                     }
                     i9++;
                 }
-                i4 = (int)i82;
+                selectedBlock = (int) tempSelectingBlock;
                 Thread.sleep(2);
                 if (!this.isActive()) {
                     return;
